1021068ae75435433c97beb1268fc38093c9cc40
ASUS <m>
2021-05-28 20:12:20 +0200

    Changed displayDatabaseInfo() method in CatalogActivity to display all data of the pets table that is currently in the database.

--- app/src/main/java/com/example/android/pets/CatalogActivity.kt
+++ app/src/main/java/com/example/android/pets/CatalogActivity.kt
@@ -17,7 +17,6 @@ package com.example.android.pets
 
 import android.content.ContentValues
 import android.content.Intent
-import android.database.Cursor
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
@@ -85,12 +84,51 @@ class CatalogActivity : AppCompatActivity() {
             null,
             null,
             null,
-            null)
-        cursor.use { cursor ->
+            null
+        )
+
+        // Always close the cursor when you're done reading from it. This releases all its
+        // resources and makes it invalid.
+        cursor?.use { cursor ->
             // Display the number of rows in the Cursor (which reflects the number of rows in the
             // pets table in the database).
-            val displayView = findViewById<View>(R.id.text_view_pet) as TextView
-            displayView.text = "Number of rows in pets database table: " + cursor?.getCount()
+            val displayView = findViewById<TextView>(R.id.text_view_pet)
+
+            // Create a header in the Text View that looks like this:
+            //
+            // The pets table contains <number of rows in Cursor> pets.
+            // _id - name - breed - gender - weight
+            //
+            // In the while loop below, iterate through the rows of the cursor and display
+            // the information from each column in this order.
+            displayView.text =
+                "The pets table contains " + cursor.count.toString() + " pets.\n\n"
+            displayView.append(
+                "${PetEntry._ID} - ${PetEntry.COLUMN_PET_NAME} - ${PetEntry.COLUMN_PET_BREED}" +
+                        " - ${PetEntry.COLUMN_PET_GENDER} - ${PetEntry.COLUMN_PET_WEIGHT}\n"
+            )
+
+            // Figure out the index of each column
+            val idColumnIndex = cursor.getColumnIndex(PetEntry._ID)
+            val nameColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_NAME)
+            val breedColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_BREED)
+            val genderColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_GENDER)
+            val weightColumnIndex = cursor.getColumnIndex(PetEntry.COLUMN_PET_WEIGHT)
+
+            // Iterate through all the returned rows in the cursor
+            while (cursor.moveToNext()) {
+                // Use that index to extract the String or Int value of the word
+                // at the current row the cursor is on.
+                val currentID = cursor.getInt(idColumnIndex)
+                val currentName = cursor.getString(nameColumnIndex)
+                val currentBreed = cursor.getString(breedColumnIndex)
+                val currentGender = cursor.getInt(genderColumnIndex)
+                val currentWeight = cursor.getInt(weightColumnIndex)
+                // Display the values from each column of the current row in the cursor in the TextView
+                displayView.append(
+                    "\n$currentID - $currentName - $currentBreed - $currentGender - $currentWeight"
+                )
+            }
         }
     }
 
