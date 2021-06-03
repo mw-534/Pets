b42dc66b739064161b343b3739759600b329b8f7
ASUS <m>
2021-06-03 15:59:03 +0200

    Implemented query() method in PetProvider class.

--- app/src/main/java/com/example/android/pets/data/PetContract.kt
+++ app/src/main/java/com/example/android/pets/data/PetContract.kt
@@ -1,6 +1,20 @@
 package com.example.android.pets.data
 
+import android.content.UriMatcher
+import android.net.Uri
 import android.provider.BaseColumns
+
+/** URI matcher code for the content URI for the pets table. */
+const val PETS = 100
+
+/** URI matcher code for the content URI for a single pet in the pets table. */
+const val PET_ID = 101
+
+val sUriMatcher = UriMatcher(UriMatcher.NO_MATCH).apply {
+    addURI(PetContract.CONTENT_AUTHORITY, PetContract.PATH_PETS, PETS)
+    addURI(PetContract.CONTENT_AUTHORITY, "${PetContract.PATH_PETS}/#", PET_ID)
+}
+
 
 /**
  * API contract for the pets app.
@@ -8,10 +22,35 @@ import android.provider.BaseColumns
 object PetContract {
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    const val CONTENT_AUTHORITY = "com.example.android.pets"
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    val BASE_CONTENT_URI = Uri.parse("content://$CONTENT_AUTHORITY")
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    const val PATH_PETS = "pets"
+
+    /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     object PetEntry : BaseColumns {
+        /** The content URI to access the pet data in the provider. */
+        val CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS)
+
         /** Name of database table for pets. */
         const val TABLE_NAME = "pets"
 
